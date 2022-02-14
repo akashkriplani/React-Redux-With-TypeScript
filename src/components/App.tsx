c0e7e2bfc4a3b3c4e211403e14696a05e3ae704b
Akash <aakash23kriplani@gmail.com>
2022-02-14 20:09:50 +0530

    Add loader

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,9 +9,25 @@ interface AppProps {
   deleteTodo: typeof deleteTodo;
 }
 
-class _App extends React.Component<AppProps> {
+interface AppState {
+  fetching: boolean;
+}
+
+class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { fetching: false };
+  }
+
+  componentDidUpdate(previousProps: AppProps): void {
+    if (!previousProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({ fetching: true });
   };
 
   onTodoClick = (id: number): void => {
@@ -32,6 +48,7 @@ class _App extends React.Component<AppProps> {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch</button>
+        <div>{this.state.fetching ? 'LOADING' : null}</div>
         {this.renderList()}
       </div>
     );
