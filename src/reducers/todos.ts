78e1563012a141b39f229ae1b212fa2619d4d177
Akash <aakash23kriplani@gmail.com>
2022-02-14 19:29:37 +0530

    Add todos Reducer in the combine reducers

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,7 +1,10 @@
 import { FetchTodosAction, Todo } from '../actions';
 import { ActionTypes } from '../actions/types';
 
-export const todosReducer = (state: Todo[], action: FetchTodosAction) => {
+export const todosReducer = (
+  state: Todo[] = [],
+  action: FetchTodosAction
+): Todo[] => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
